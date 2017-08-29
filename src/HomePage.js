2a51e2c520af0a69347844a56031bd28b7eb9077
jylei <jlei91@gmail.com>
2017-08-29 16:21:17 -0700

    homepage

---
+++ src/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
+import { withRouter,Link } from 'react-router-dom'
+import './HomePage.css'
+class HomePage extends React.Component {
+    render() {
+        return (
+            <nav>
+                <Link onClick={this.forceUpdate} to={'/recipepage'}>
+                <h1>test</h1>
+                </Link>
+            </nav>
+
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>
+    ({
+        getRecipe: () => dispatch(push('/recipepage'))
+    })
+
+export default withRouter(connect(null, mapDispatchToProps)(HomePage));
