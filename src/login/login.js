c307baff3c0ccbc8772dda7dfbf175041c22991d
jylei <jlei91@gmail.com>
2017-09-07 08:55:33 -0700

    login

--- src/login/login.js
+++ src/login/login.js
@@ -1,43 +1,69 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
-import { login } from './reducer';
+import { login } from '../reducer';
+import { Redirect, withRouter } from 'react-router-dom';
 
-const LogIn = (props) => {
-    let username, password;
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(`INSIDE HANDLESUBMIT--- ${username.value}, ${password.value}`);
-        return props.login({
-            username: username.value,
-            password: password.value
-        });
+class LogIn extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loginSuccess: false
+        };
     }
 
-    return (
-        <div>
-            <header>
+    componentWillReceiveProps(newProps) {
+        console.log('Component WILL RECIEVE PROPS!')
+        console.log(newProps.isLoggedIn);
+        if (newProps.isLoggedIn === true) {
+            this.setState({ loginSuccess: true })
+            this.props.goToHomePage();
+        }
+    }
+    handleSubmit(e) {
+        let username, password;
+        e.preventDefault();
+        console.log(`handlesubmit--- ${this.username.value} ${this.password.value}`);
+        console.log(this.props.isLoggedIn);
+        // if state.logged = false, return props login , else redirect
+        return this.props.login({
+            username: this.username.value,
+            password: this.password.value
+        })
+    }
+    render() {
+        if (this.state.loginSuccess) {
+            return <Redirect to='/homepage' />;
+        }
+        return (
+            <div>
+                <form onSubmit={e => this.handleSubmit(e)} id="login-form">
+                    <div className="login">
+                        <p><input type="text" ref={(input) => this.username = input} className="blank" placeholder="User ID" size="35" required /></p>
+                        <p><input type="password" ref={(input) => this.password = input} className="blank" placeholder="Password" size="35" required /></p>
+                        <p className="login_button">
+                            <button className="signup-login-button" >Log In</button>
+                        </p>
+                    </div>
+                </form>
                 <p className="header-login-button">
-                    <button className="page-login-signup-button" onClick={props.gotoSignup}>Sign Up</button>
+                    <button className="page-login-signup-button" onClick={this.props.gotoSignup}>Sign Up</button>
                 </p>
-            </header>
-            <form onSubmit={handleSubmit} id="login-form">
-                <div className="black-box login">
-                    <p><input type="text" ref={(input) => username = input} className="blank" placeholder="User ID" size="35" required /></p>
-                    <p><input type="password" ref={(input) => password = input} className="blank" placeholder="Password" size="35" required /></p>
-                    <p className="login_button">
-                        <button className="signup-login-button" >Log In</button>
-                    </p>
-                </div>
-            </form>
-        </div>
-    );
+            </div>
+        );
+    }
 }
+
 const mapDispatchToProps = (dispatch) => ({
     login: (attributes) => {
         console.log("mapDispatchToProps logging out creds", attributes);
         dispatch(login(attributes));
     },
-    gotoSignup: () => dispatch(push('/signup'))
+    gotoSignup: () => dispatch(push('/signup')),
+    goToHomePage: () => dispatch(push('/homepage'))
 })
-export default connect(null, mapDispatchToProps)(LogIn)
+
+const mapStateToProps = (state) => ({
+    isLoggedIn: state.isLoggedIn
+})
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(LogIn));
--- src/login/reducer.js
+++ src/login/reducer.js
@@ -2,26 +2,33 @@ import { API_BASE_URL } from '../config'
 import { push } from 'react-router-redux';
 
 const initialState = {
-    isLoggedIn: false
+    isLoggedIn: false,
+    secretWord: ''
 }
 
-const LOGIN_FINISHED = (state, action) => {
-    return Object.assign({}, state,
-        {
-            token: action.payload,
-            isLoggedIn: true
-        });
-}
-
+export const LOGIN_FINISHED = 'LOGIN_FINISHED';
 export const loginFinished = (cred) => ({
     type: LOGIN_FINISHED,
     payload: cred
 })
 
+//reducxcer check for type ofaction, check payload for the state which is okay, then isloggedin:true, then compnent will render data based on state
+
+export const loginReducer = (state = initialState, action) => {
+    if (action.type === LOGIN_FINISHED) {
+        console.log(action.payload) 
+        state = Object.assign({}, state, {
+            secretWord:action.payload
+        })
+        console.log(state);
+        return state;
+    }
+    return state;
+}
 
 export const login = (data) => (dispatch) => {
     console.log("login credentials", data)
-    fetch(`http://localhost:8080/api/users/login`, {
+    fetch('http://localhost:8080/api/users/login', {
         method: 'post',
         headers: {
             'Accept': 'application/json',
@@ -29,14 +36,13 @@ export const login = (data) => (dispatch) => {
         },
         body: JSON.stringify(data)      //username, password
     }).then(res => {
-        console.log(res.json())
+        //json info in here from server
+        // console.log(res.json());
         if (!res.ok) {
             return Promise.reject(res.statusText);
         }
-        return res.json();
+        res.json().then((data) => {
+            dispatch(loginFinished(data.status));
+        })
     })
-        .then(res => {
-            console.log(res);
-            dispatch(loginFinished(res));
-        }).catch(err => console.log(err))
 }
