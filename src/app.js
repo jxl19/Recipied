5f6a4d00a5eba282d1a318f03f12922b3b9e1fa4
jylei <jlei91@gmail.com>
2017-09-07 08:55:03 -0700

    routes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,49 @@
 import React from 'react';
-// import {Switch,Route} from 'react-router-dom';
 import { connect } from "react-redux";
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 import GetRecipe from './getRecipe';
 import RecipeCreatePage from './recipecreate-page';
 import RecipePage from './recipePage';
 import LogIn from './login/login';
 import HomePage from './HomePage';
-export class App extends React.Component {
+import { createBrowserHistory } from 'history';
+
+class App extends React.Component {
 
     render() {
+        let loggedIn = this.props.isLoggedIn;
+        console.log(loggedIn);
         return (
-
             <div className="app">
-                {/* <Switch> */}
-                {/* < RecipeCreatePage /> */}
-                {/*getrecipe ep no need*/}
-                {/* <Route path ="/getrecipe" component={GetRecipe} /> */}
-                {/* <p className="recipePage">
+                <Router history={createBrowserHistory}>
+                    <Switch>
+                        {/* < RecipeCreatePage /> */}
+                        {/*getrecipe ep no need*/}
+                        {/* <Route path ="/getrecipe" component={GetRecipe} /> */}
+                        {/* <p className="recipePage">
                     <Link to="recipepage">Recipes</Link>  
                 </p> */}
-                <Route path ="/homepage" component={HomePage} />
-                {/* <Route path ="/login" component ={LogIn} /> */}
-                <Route path="/recipepage" component={RecipePage} />
-                {/* <RecipePage /> */}
-                <Route exact path="/login" component={LogIn} />
-                {/* </Switch> */}
+                        <Route path="/homepage" component={HomePage} />
+                        <Route path="/recipepage" component={RecipePage} />
+
+                        <Route exact path="/login" render={() => (
+                        loggedIn ? (
+                            <Redirect to="/homepage" />
+                        ) : (
+                                <LogIn />
+                            )
+                    )} />
+                        {/* <Route path="/login" component={LogIn} /> */}
+                    </Switch>
+                </Router>
             </div>
 
         );
     }
 }
 
+const mapStateToProps = (state) => ({
+    isLoggedIn: state.isLoggedIn
+})
 
-export default connect()(App);
+export default connect(mapStateToProps)(App);
