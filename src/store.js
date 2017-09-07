e3b57eb68a136b8c461b07b3e2a9dae3747eeb71
jylei <jlei91@gmail.com>
2017-09-07 08:56:38 -0700

    store

--- src/store.js
+++ src/store.js
@@ -1,15 +1,23 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
-import { routerMiddleware } from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware, push } from 'react-router-redux';
 import {recipeReducer} from './reducer';
 
-export const history = createHistory();
+import {createBrowserHistory} from 'history';
+
+const history = createBrowserHistory()
 
 const middleware = [
     thunk,
     routerMiddleware(history)
 ];
+
+// const reducer = combineReducers(
+//     {
+//         loginReducer,
+//         recipeReducer
+//     }
+// )
 
 export default createStore(recipeReducer, applyMiddleware(...middleware));
 
