ffb523e7fcc17935726d70b98e7cb13134f65c9e
jylei <jlei91@gmail.com>
2018-02-09 20:43:38 -0800

    bitly

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
     process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-export const BITLY_URL = process.env.BITLY_URL;
+export const BITLY_URL = process.env.REACT_APP_BITLY_URL;
