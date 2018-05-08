19bd788d5195f3b1175032e1f13b3e0e59b0eb28
jylei <jlei91@gmail.com>
2018-05-07 23:00:22 -0700

    ingredients seperated by commas

--- src/components/recipePage.js
+++ src/components/recipePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getReciped, getUserName, getAllRecipes } from '../reducers/reducer';
-import {getId} from '../actions/action';
+import { getId } from '../actions/action';
 import './recipePage.css';
 import { Table, Column, Cell } from 'fixed-data-table';
 import DashBoard from './DashBoard';
@@ -14,22 +14,22 @@ class RecipePage extends React.Component {
         super(props);
         this.state = { width: '0', height: '0' };
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-        
-      }
-      
-      componentDidMount() {
+
+    }
+
+    componentDidMount() {
         this.props.dispatch(getAllRecipes());
         this.updateWindowDimensions();
         window.addEventListener('resize', this.updateWindowDimensions);
-      }
-      
-      componentWillUnmount() {
+    }
+
+    componentWillUnmount() {
         window.removeEventListener('resize', this.updateWindowDimensions);
-      }
-      
-      updateWindowDimensions() {
+    }
+
+    updateWindowDimensions() {
         this.setState({ width: window.innerWidth, height: window.innerHeight });
-      }
+    }
     onSubmit(e) {
         e.preventDefault();
         var userid = sessionStorage.getItem('id');
@@ -46,11 +46,37 @@ class RecipePage extends React.Component {
             history.push('/recipepage/' + this.props.id);
             return <Redirect to={"/recipepage/" + this.props.id} />
         }
+        let newStr;
+        function removeArr(str) {
+            for(var i = 0; i < str.length; i++) {
+            newStr = str[i];
+            }
+            newStr = newStr.split('-');
+        }
+        function splitString(str) {
+            removeArr(str);
+            var arr = [];
+            let split;
+            for (var i = 0; i < newStr.length; i++) {
+                split = newStr[i].replace('↵', '');
+                if (newStr[i] !== '') {
+                    arr.push(newStr[i].split(/\n|\r|↵/).join( '' ));
+                }
+            }
+            let addedHTML='';
+            for(var i = 0; i < arr.length; i++) {
+                if(i === 0) {
+                    addedHTML+= `${arr[i]}`;
+                }
+                else addedHTML += `, ${arr[i]}`;
+            }
+            return addedHTML;
+        }
         var userid = sessionStorage.getItem('id');
         let recipes = undefined;
-        let tableWidth = this.state.width*0.98;
-        let columnWidth = this.state.width*0.49;
-        let tableHeight = (this.props.existingRecipes.length + 1) *69.8;
+        let tableWidth = this.state.width * 0.98;
+        let columnWidth = this.state.width * 0.49;
+        let tableHeight = (this.props.existingRecipes.length + 1) * 69.8;
         if (this.props.existingRecipes.length > 0) {
             recipes =
                 <Table className="centerdiv"
@@ -72,7 +98,7 @@ class RecipePage extends React.Component {
                         header={<Cell>Ingredients</Cell>}
                         cell={props => (
                             <Cell {...props}>
-                                {this.props.existingRecipes[props.rowIndex].ingredients}
+                                {splitString(this.props.existingRecipes[props.rowIndex].ingredients)}
                             </Cell>
                         )}
                         width={columnWidth}
